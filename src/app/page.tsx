307f3f54affc5119713c5c780465fb38ca30f22f
btreitz <basti.treitz@gmail.com>
2023-07-01 16:05:01 +0200

    Improve hero typography

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,17 @@ export default function Home() {
 	return (
 		<div className=" flex flex-col w-full">
 			<div className=" border w-full relative">
-				<div className=" absolute top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 flex flex-col items-center text-center text-primary">
-					<h1 className=" text-5xl font-bold">Vörðr Sæde</h1>
-					<p className=" pb-8">
+				<div className=" absolute top-[30%] left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 flex flex-col items-center text-center text-primary w-3/4 sm:max-w-2xl">
+					<h1 className=" text-6xl font-bold tracking-tight pb-11">Vörðr Sæde</h1>
+					<p className=" pb-6">
 						Transform your living space with the captivating allure of Scandinavian design - discover the perfect blend
 						of timeless elegance and functional simplicity.
 					</p>
 					<Link
 						href={{ pathname: '/inventory' }}
-						className=" w-56 p-3 backdrop-blur border border-black rounded-full transition ease-out duration-200 text-center hover:bg-white/50 hover:scale-105"
+						className=" w-56 p-3 backdrop-blur border border-black rounded-full transition ease-out duration-200 text-center hover:bg-white/50 hover:scale-105 tracking-widest"
 					>
-						<div>EXPLORE</div>
+						explore
 					</Link>
 				</div>
 				<Image
