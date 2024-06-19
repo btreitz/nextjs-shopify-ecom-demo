138e53f4013353bcc5553ff72aac226a40d8dcff
btreitz <basti.treitz@gmail.com>
2024-06-19 23:54:24 +0200

    Update all version

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,7 +1,4 @@
 import Cart from '@/components/cart/cart';
-import CartProductOverview from '@/components/cart/cartProductOverview';
-import OrderSummary from '@/components/cart/orderSummary';
-import OrderSummaryMobile from '@/components/cart/orderSummaryMobile';
 import RedirectToCartProducts from '@/components/redirects/redirectToCartProducts';
 
 type Props = {
